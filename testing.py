2feb186846ab1d1ac47c5f763423a9f8826b0c16
Jose N <jmnunezd123@gmail.com>
2018-10-02 17:26:36 -0400

    Revert "new features add: * now you can see a result dataframe with the info of the 120 largest   cities in world. using request_weather_list. * now you can insert any pair of lat, lon and seek for results."
    This reverts commit 5971c0e

--- testing.py
+++ testing.py
@@ -1,55 +1,28 @@
 from credentials import key
 import pandas as pd
-from urllib import request
-import ast
-
 pd.options.display.max_columns = 30
 pd.set_option("display.width", 1000)
+from urllib import request
+import ast  # this library has a method to make str into dictionaries
+
+# page_url: https://darksky.net/dev/docs
 
 key = key()
 
-# --- Wrangling the data:
-coordf = pd.read_csv("latitude&longitude.csv", sep=";")
-coordf["lat_card"] = coordf["lat1"].apply(lambda row: row[-1])
-coordf["lon_card"] = coordf["lon1"].apply(lambda row: row[-1])
-coordf["lat"] = coordf["lat_card"].apply(lambda row: 1 if row == "N" else -1) * \
-      (coordf["lat0"].astype("str") + "." + coordf["lat1"].apply(lambda row: row[:-2])).astype("float64")
-coordf["lon"] = coordf["lon_card"].apply(lambda row: 1 if row == "E" else -1) * \
-      (coordf["lon0"].astype("str") + "." + coordf["lon1"].apply(lambda row: row[:-2])).astype("float64")
-coordf = coordf[["City", "lat", "lon"]]
+weather1 = "https://api.darksky.net/forecast/" + key + "/10.29, -66.52"  # Caracas
+weather2 = "https://api.darksky.net/forecast/" + key + "/4.35, -74.04"  # Bogota
 
-# --- Creating a class that will handle everything:
-class Coordinates(object):
-    def __init__(self, latitude, longitude):
-        self.lat = latitude
-        self.lon = longitude
+resp1 = request.urlopen(weather1).read().decode("UTF-8")
+resp2 = request.urlopen(weather2).read().decode("UTF-8")
 
-    def request_weather(self):
-        weather = "https://api.darksky.net/forecast/" + key + "/%s,%s?units=ca" % (self.lat, self.lon)
-        resp = request.urlopen(weather).read().decode("UTF-8")
-        resp = ast.literal_eval(resp)
-        respdf = pd.DataFrame(resp["daily"]["data"])
-        return respdf
+resp1 = ast.literal_eval(resp1)  # dictionary
+resp2 = ast.literal_eval(resp2)  # dictionary
 
-    def request_weather_list(self):
-        cities = pd.DataFrame()
-        for idx in coordf.index:
-            slice = coordf.iloc[idx]
-            lat = slice["lat"]
-            lon = slice["lon"]
-            city = Coordinates(lat, lon)
-            city = city.request_weather()
-            city["City"] = slice["City"]
-            cities = cities.append(city)
-        cities.reset_index(inplace=True)
-        cities.rename(columns={"index": "days"}, inplace=True)
-        cities.to_csv("global_weather_results.csv")
-        return cities
+respdf1 = pd.DataFrame(resp1["daily"]["data"])
+respdf1["city"] = "Caracas"
+respdf2 = pd.DataFrame(resp2["daily"]["data"])
+respdf2["city"] = "Bogota"
 
-# eg1:
-san_francisco = Coordinates("37.72", "123.03")
-print(san_francisco.request_weather())
+print(respdf1)
+print(respdf2)
 
-#eg2:
-all = Coordinates(0, 0)
-print(all.request_weather_list())
